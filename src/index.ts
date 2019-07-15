9f160930a0ebbff64cf31313d5b7492f9404775b
Kyle Soeltz <skiis652@gmail.com>
2019-07-15 19:55:17 -0400

    Debugging heroku

--- src/index.ts
+++ src/index.ts
@@ -12,22 +12,22 @@ app.use(function(req, res, next) {
 app.get('/', (req, res, next) => {
   res.send('Welcome to Notches');
 });
-app.get('/api/test', (req, res, next) => {
-  res.send({message: 'Notches'});
-});
+// app.get('/api/test', (req, res, next) => {
+//   res.send({message: 'Notches'});
+// });
 
-if (process.env.NODE_ENV == 'production') {
-  // Express will serve up production assets
-  // like our main.js or main.css file
-  app.use(express.static('client/build'))
+// if (process.env.NODE_ENV == 'production') {
+//   // Express will serve up production assets
+//   // like our main.js or main.css file
+//   app.use(express.static('client/build'))
 
-  // Express will serve up index.html if it
-  // does not recognize the route
-  const path = require('path');
-  app.get('*', (req, res) => {
-    res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
-  })
-}
+//   // Express will serve up index.html if it
+//   // does not recognize the route
+//   const path = require('path');
+//   app.get('*', (req, res) => {
+//     res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
+//   })
+// }
 
 const PORT = process.env.PORT || 5000;
 
