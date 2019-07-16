de180c0612741606bc78682f24929e43568e06a8
Kyle Soeltz <skiis652@gmail.com>
2019-07-15 20:01:02 -0400

    heroku debug

--- src/index.ts
+++ src/index.ts
@@ -3,11 +3,11 @@ import express from 'express';
 const app = express();
 
 // Set this to allow cross origin access
-app.use(function(req, res, next) {
-  res.header("Access-Control-Allow-Origin", "*");
-  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
-  next();
-});
+// app.use(function(req, res, next) {
+//   res.header("Access-Control-Allow-Origin", "*");
+//   res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+//   next();
+// });
 
 app.get('/', (req, res, next) => {
   res.send('Welcome to Notches');
