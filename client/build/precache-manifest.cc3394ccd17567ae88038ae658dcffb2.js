d9612de7b87cd889fd819018d326ef5d5696ec5b
Kyle Soeltz <skiis652@gmail.com>
2019-07-15 20:17:37 -0400

    heroku debug -add built client files to git

---
+++ client/build/precache-manifest.cc3394ccd17567ae88038ae658dcffb2.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "10be91d9287c8f26ad98dd19c67c89ec",
+    "url": "/index.html"
+  },
+  {
+    "revision": "08a41706b0e16d1b1bd7",
+    "url": "/static/css/main.19393e92.chunk.css"
+  },
+  {
+    "revision": "f338f087be30be6f9078",
+    "url": "/static/js/2.3e6137d4.chunk.js"
+  },
+  {
+    "revision": "08a41706b0e16d1b1bd7",
+    "url": "/static/js/main.fbabb24a.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  }
+]);
