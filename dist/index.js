51e5cc73f9212117012f58eb7b5329fbd2648b0c
Kyle Soeltz <skiis652@gmail.com>
2019-07-15 20:14:00 -0400

    heroku debug

--- dist/index.js
+++ dist/index.js
@@ -4,28 +4,28 @@ var tslib_1 = require("tslib");
 var express_1 = tslib_1.__importDefault(require("express"));
 var app = express_1.default();
 // Set this to allow cross origin access
-// app.use(function(req, res, next) {
-//   res.header("Access-Control-Allow-Origin", "*");
-//   res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
-//   next();
-// });
-app.get('/', function (req, res, next) {
-    res.send('Welcome to Notches');
+app.use(function (req, res, next) {
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+    next();
 });
-// app.get('/api/test', (req, res, next) => {
-//   res.send({message: 'Notches'});
+// app.get('/', (req, res, next) => {
+//   res.send('Welcome to Notches');
 // });
-// if (process.env.NODE_ENV == 'production') {
-//   // Express will serve up production assets
-//   // like our main.js or main.css file
-//   app.use(express.static('client/build'))
-//   // Express will serve up index.html if it
-//   // does not recognize the route
-//   const path = require('path');
-//   app.get('*', (req, res) => {
-//     res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
-//   })
-// }
+app.get('/api/test', function (req, res, next) {
+    res.send({ message: 'Notches' });
+});
+if (process.env.NODE_ENV == 'production') {
+    // Express will serve up production assets
+    // like our main.js or main.css file
+    app.use(express_1.default.static('client/build'));
+    // Express will serve up index.html if it
+    // does not recognize the route
+    var path_1 = require('path');
+    app.get('*', function (req, res) {
+        res.sendFile(path_1.resolve(__dirname, 'client', 'build', 'index.html'));
+    });
+}
 var PORT = process.env.PORT || 5000;
 app.listen(PORT, function () {
     // tslint:disable-next-line
--- src/index.ts
+++ src/index.ts
@@ -3,31 +3,31 @@ import express from 'express';
 const app = express();
 
 // Set this to allow cross origin access
-// app.use(function(req, res, next) {
-//   res.header("Access-Control-Allow-Origin", "*");
-//   res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
-//   next();
-// });
-
-app.get('/', (req, res, next) => {
-  res.send('Welcome to Notches');
+app.use(function(req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+  next();
 });
-// app.get('/api/test', (req, res, next) => {
-//   res.send({message: 'Notches'});
+
+// app.get('/', (req, res, next) => {
+//   res.send('Welcome to Notches');
 // });
+app.get('/api/test', (req, res, next) => {
+  res.send({message: 'Notches'});
+});
 
-// if (process.env.NODE_ENV == 'production') {
-//   // Express will serve up production assets
-//   // like our main.js or main.css file
-//   app.use(express.static('client/build'))
+if (process.env.NODE_ENV == 'production') {
+  // Express will serve up production assets
+  // like our main.js or main.css file
+  app.use(express.static('client/build'))
 
-//   // Express will serve up index.html if it
-//   // does not recognize the route
-//   const path = require('path');
-//   app.get('*', (req, res) => {
-//     res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
-//   })
-// }
+  // Express will serve up index.html if it
+  // does not recognize the route
+  const path = require('path');
+  app.get('*', (req, res) => {
+    res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
+  })
+}
 
 const PORT = process.env.PORT || 5000;
 
