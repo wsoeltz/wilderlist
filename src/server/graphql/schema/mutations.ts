87350291e1a2ad66566fe9d8265dee182b833baa
Kyle Soeltz <kyle_soeltz@hks.harvard.edu>
2019-07-23 17:05:53 -0400

    begin work on delete mutations

--- src/server/graphql/schema/mutations.ts
+++ src/server/graphql/schema/mutations.ts
@@ -4,7 +4,12 @@ import {
   GraphQLObjectType,
   GraphQLString,
 } from 'graphql';
-import RegionType, { Region, RegionModelType } from './regionType';
+import mongoose from 'mongoose';
+// import { State as StateInterface } from '../graphQLTypes';
+import RegionType, { Region } from './regionType';
+import /*StateType,*/ { State } from './stateType';
+
+mongoose.set('useFindAndModify', false);
 
 const mutation = new GraphQLObjectType({
   name: 'Mutation',
@@ -15,10 +20,37 @@ const mutation = new GraphQLObjectType({
         name: { type: GraphQLString },
         states: { type: new GraphQLList(GraphQLID)},
       },
-      resolve(parentValue, { name, states }: RegionModelType) {
-        return (new Region({ name, states })).save();
+      resolve(parentValue, { name, states }) {
+        const newRegion = new Region({ name, states });
+        states.forEach((id: string[]) => {
+          State.findByIdAndUpdate(id,
+            { $push: {regions: newRegion.id} },
+            function(err, model) {
+              if (err) {
+                console.error(err);
+              }
+            },
+          );
+        });
+        return newRegion.save();
       },
     },
+    // deleteRegion: {
+    //   type: RegionType,
+    //   args: {
+    //     id: { type: GraphQLID },
+    //   },
+    //   resolve(parentValue, { id }) {
+    //     State.updateMany(
+    //       { $elemMatch: {regions: id} },
+    //       { $pull: {regions: id} },
+    //       (err, raw) => {
+    //         console.log({err, raw});
+    //       }
+    //     );
+    //     return Region.findByIdAndDelete(id);
+    //   },
+    // },
   },
 });
 
