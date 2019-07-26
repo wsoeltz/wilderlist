1814d3b4a1433827e883931f2aacd8903d59c1ac
Kyle Soeltz <kyle_soeltz@hks.harvard.edu>
2019-07-26 13:33:16 -0400

    add typescript linting and configuration to client directory

--- client/src/components/App.tsx
+++ client/src/components/App.tsx
@@ -3,7 +3,7 @@ import React from 'react';
 import { ApolloProvider } from 'react-apollo';
 import AdminPanel from './adminPanel';
 
-const BASE_URL = process.env.REACT_APP_API_BASE_URL
+const BASE_URL = process.env.REACT_APP_API_BASE_URL;
 const uri = BASE_URL === undefined ? '/graphql' : `${BASE_URL}/graphql`;
 
 const client = new ApolloClient({ uri });
