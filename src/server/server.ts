bfcd7bdf76b437a85302a01f0410bd7e0ec027bb
Kyle Soeltz <kyle_soeltz@hks.harvard.edu>
2019-07-29 17:43:59 -0400

    fix path issues preventing proper routing in production

--- src/server/server.ts
+++ src/server/server.ts
@@ -49,7 +49,7 @@ if (process.env.NODE_ENV === 'production') {
   // does not recognize the route
   const path = require('path');
   app.get('*', (req, res) => {
-    res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
+    res.sendFile(path.resolve(__dirname, '../../client', 'build', 'index.html'));
   });
 }
 
