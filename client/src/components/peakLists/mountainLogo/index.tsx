0cf4cb9b2168583be0dd5a60e03a85603094666b
Kyle Soeltz <kyle_soeltz@hks.harvard.edu>
2019-08-27 17:44:00 -0400

    Style text for mountain logos

--- client/src/components/peakLists/mountainLogo/index.tsx
+++ client/src/components/peakLists/mountainLogo/index.tsx
@@ -6,41 +6,35 @@ import { PeakListVariants } from '../../../types/graphQLTypes';
 import Badge from './Badge';
 
 interface StyleProps {
-  id: string;
   colorSet: ColorSet;
+}
+interface ShortNameProps {
+  colorSet: ColorSet;
+  shortNameSize: string;
+  shortNameStroke: string;
 }
 
 export const classNames = {
   mountainExterior: 'mountain-exterior',
   mountainInterior: 'mountain-interior',
   sky: 'emblem-sky',
+  mountainLogoTitleText: 'mountain-logo-title-text',
+  variantName: 'variant-name',
+  shortName: 'short-name',
 };
 
-const SVG = styled.svg<StyleProps>`
-  font-family: DeliciousWeb;
+const Root = styled.div`
+  position: relative;
+`;
 
-  .mountainLogoTitleText {
+const SVG = styled.svg<StyleProps>`
+  width: 100%;
+
+  .${classNames.mountainLogoTitleText} {
+    font-family: DeliciousWeb;
     font-size: 73px;
     text-align: center;
     fill: ${({colorSet}) => colorSet.primary};
-  }
-
-  .variant-name {
-    font-size: 102.73px;
-    fill: ${({colorSet}) => colorSet.primary};
-  }
-
-  .short-name {
-    font-family: DeliciousWeb;
-    font-size: 218.43px;
-    fill: #fff;
-    stroke: ${({colorSet}) => colorSet.primary};
-    stroke-miterlimit: 10;
-    stroke-width: 8px;
-  }
-
-  .clear-fill {
-    fill: none;
   }
 
   .${classNames.mountainExterior} {
@@ -62,6 +56,36 @@ const SVG = styled.svg<StyleProps>`
   }
 `;
 
+const ShortName = styled.svg<ShortNameProps>`
+  width: 100%;
+  position: absolute;
+  left: 0;
+  right: 0;
+  bottom: 15%;
+
+  text {
+    font-family: DeliciousWeb;
+    font-size: ${({shortNameSize}) => shortNameSize};
+    fill: #fff;
+    stroke: ${({colorSet}) => colorSet.primary};
+    stroke-width: ${({shortNameStroke}) => shortNameStroke}px;
+  }
+`;
+const VariantName = styled.svg<StyleProps>`
+  width: 100%;
+  position: absolute;
+  left: 0;
+  right: 0;
+  bottom: 0;
+
+  text {
+    font-size: 0.45rem;
+    font-family: DeliciousWeb;
+    text-transform: uppercase;
+    fill: ${({colorSet}) => colorSet.primary};
+  }
+`;
+
 interface Props {
   id: string;
   title: string;
@@ -72,33 +96,58 @@ interface Props {
 const MountainLogo = (props: Props) => {
   const { id, title, shortName, variant } = props;
   const titleId = 'mountainLogoTitle-' + id;
-  return (
-    <SVG
-      id={id}
-      colorSet={getColorSetFromVariant(variant)}
-      xmlns='http://www.w3.org/2000/svg'
-      viewBox='0 0 820.33 820.33'
+  const colorSet = getColorSetFromVariant(variant);
+  const shortNameSize = shortName.length > 7 ? '0.7rem' : '1rem';
+  const shortNameStroke = shortName.length > 7 ? '0.5' : '0.7';
+  const variantName = variant === PeakListVariants.standard ? null : (
+    <VariantName
+      colorSet={colorSet}
+      viewBox="0 0 56 18"
+      textAnchor='middle'
     >
-      <defs>
-        <path id={titleId} fill='none' d='M184.91,609.63A305,305,0,0,1,104,402.33C104,233.26,241.1,96.21,410.16,96.21S716.29,233.26,716.29,402.33a305,305,0,0,1-76,201.88'
-        x='0' y='0'/>
-      </defs>
-
-      <Badge id={id} />
-
-      <text>
-        <textPath
-          href={`#${titleId}`}
-          className='mountainLogoTitleText'
-          startOffset='50%'
-          textAnchor='middle'
-        >
-          {title}
-        </textPath>
+      <text x="50%" y="90%">
+        {variant}
       </text>
-      <text className={`variant-name`} transform='translate(316.73 787.36)'>{variant}</text>
-      <text className={`short-name`} transform='translate(100.14 675.48)'>{shortName}</text>
-    </SVG>
+    </VariantName>
+  );
+  return (
+    <Root>
+      <SVG
+        colorSet={colorSet}
+        xmlns='http://www.w3.org/2000/svg'
+        viewBox='0 0 820.33 820.33'
+      >
+        <defs>
+          <path id={titleId} fill='none' d='M184.91,609.63A305,305,0,0,1,104,402.33C104,233.26,241.1,96.21,410.16,96.21S716.29,233.26,716.29,402.33a305,305,0,0,1-76,201.88'
+          x='0' y='0'/>
+        </defs>
+
+        <Badge id={id} />
+
+        <text>
+          <textPath
+            href={`#${titleId}`}
+            className={classNames.mountainLogoTitleText}
+            startOffset='50%'
+            textAnchor='middle'
+          >
+            {title}
+          </textPath>
+        </text>
+      </SVG>
+      <ShortName
+        colorSet={colorSet}
+        viewBox="0 0 56 18"
+        textAnchor='middle'
+        shortNameSize={shortNameSize}
+        shortNameStroke={shortNameStroke}
+      >
+        <text x="50%" y="70%">
+          {shortName}
+        </text>
+      </ShortName>
+      {variantName}
+    </Root>
   );
 };
 
