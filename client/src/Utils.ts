f9f192a8411a86e27a2ee610a5801a0c3239a0bb
Kyle Soeltz <wsoeltz@gmail.com>
2020-01-06 16:26:49 -0500

    Add additional mountain selection to trip report modal

--- client/src/Utils.ts
+++ client/src/Utils.ts
@@ -310,3 +310,33 @@ export const states = [
 export const roundPercentToSingleDecimal = (numerator: number, denominator: number) => {
   return Math.round((100 * (numerator / denominator)) * 10) / 10;
 };
+
+/* distance formula from
+https://stackoverflow.com/
+questions/18883601/function-to-calculate-distance-between-two-coordinates
+*/
+interface DistanceInput {
+  lat1: number;
+  lon1: number;
+  lat2: number;
+  lon2: number;
+}
+
+const deg2rad = (deg: number) => deg * (Math.PI / 180);
+
+const getDistanceFromLatLonInKm = ({lat1, lon1, lat2, lon2}: DistanceInput) => {
+  const R = 6371; // Radius of the earth in km
+  const dLat = deg2rad(lat2 - lat1);  // deg2rad below
+  const dLon = deg2rad(lon2 - lon1);
+  const a =
+    Math.sin(dLat / 2) * Math.sin(dLat / 2) +
+    Math.cos(deg2rad(lat1)) * Math.cos(deg2rad(lat2)) *
+    Math.sin(dLon / 2) * Math.sin(dLon / 2)
+    ;
+  const c = 2 * Math.atan2(Math.sqrt(a), Math.sqrt(1 - a));
+  const d = R * c; // Distance in km
+  return d;
+};
+
+export const getDistanceFromLatLonInMiles = (input: DistanceInput) =>
+  getDistanceFromLatLonInKm(input) * 0.62137;
