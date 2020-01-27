f835f58213512994e0dd8280a07a3d96b2657715
Kyle Soeltz <wsoeltz@gmail.com>
2020-01-27 10:30:13 -0500

    Move buildDataLoaders outside of context to improve network speeds

--- src/server/server.ts
+++ src/server/server.ts
@@ -72,12 +72,13 @@ mongoose.connection
     .on('error', error => console.log('Error connecting to MongoLab:', error));
 
 const graphiql = process.env.NODE_ENV === 'development' ? true : false;
+const dataloaders = buildDataloaders();
 app.use(bodyParser.json());
 app.use('/graphql', requireLogin, expressGraphQL((req: any) => ({
   schema,
   graphiql,
   context: {
-    dataloaders: buildDataloaders(),
+    dataloaders,
     user: req.user,
   },
 })));
