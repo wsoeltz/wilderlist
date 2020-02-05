1c9d0232595e84d6e3fcfd14c8cf117a74b220ad
Kyle Soeltz <wsoeltz@gmail.com>
2020-02-05 17:38:27 -0500

    Fix lag on user notes field

--- client/src/components/sharedComponents/UserNote.tsx
+++ client/src/components/sharedComponents/UserNote.tsx
@@ -4,8 +4,7 @@ import {
 } from '@fortawesome/free-brands-svg-icons';
 import { GetString } from 'fluent-react';
 import {rgba} from 'polished';
-import React, {useContext, useState} from 'react';
-import TextareaAutosize from 'react-autosize-textarea';
+import React, {useContext, useEffect, useRef, useState} from 'react';
 import styled from 'styled-components';
 import {
   AppLocalizationAndBundleContext,
@@ -27,12 +26,13 @@ import {
   redditRed,
 } from '../login';
 
-const Textarea = styled(TextareaAutosize)`
+const Textarea = styled.textarea`
   padding: 0.5rem;
   box-sizing: border-box;
   font-size: 1rem;
   font-weight: ${lightFontWeight};
   width: 100%;
+  min-height: 4.75rem;
   line-height: 1.5;
   border: solid 1px ${rgba(lightBorderColor, 0.6)};
   outline: none;
@@ -75,13 +75,26 @@ interface Props {
 const UserNote = (props: Props) => {
   const {placeholder, defaultValue, onSave} = props;
   const [value, setValue] = useState<string>(defaultValue);
+  const [height, setHeight] = useState<number>(0);
+  const textAreaRef = useRef<HTMLTextAreaElement | null>(null);
+
+  useEffect(() => {
+    if (textAreaRef && textAreaRef.current) {
+      setHeight(textAreaRef.current.scrollHeight);
+    }
+  }, [textAreaRef, defaultValue]);
 
   const user = useContext(UserContext);
 
   const {localization} = useContext(AppLocalizationAndBundleContext);
   const getFluentString: GetString = (...args) => localization.getString(...args);
 
-  const onChange = (e: React.ChangeEvent<HTMLTextAreaElement>) => setValue(e.target.value);
+  const onChange = (e: React.ChangeEvent<HTMLTextAreaElement>) => {
+    setValue(e.target.value);
+    if (e.target.scrollHeight > height) {
+      setHeight(e.target.scrollHeight);
+    }
+  };
   const buttonText = defaultValue !== value ? 'Save Changes' : 'All Changes Saved';
 
   const buttons = !user ? (
@@ -123,9 +136,9 @@ const UserNote = (props: Props) => {
         placeholder={placeholder}
         value={value}
         onChange={onChange}
-        rows={2}
-        async={true}
+        style={{height}}
         maxLength={charLimit}
+        ref={textAreaRef}
       />
       {buttons}
     </>
