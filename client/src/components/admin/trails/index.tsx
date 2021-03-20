e94ffbd1a54b4b9557c79f4d2e336c9ae9e0d0a4
Kyle Soeltz <wsoeltz@gmail.com>
2021-03-20 11:06:10 -0400

    Add trails to admin panel

--- client/src/components/admin/trails/index.tsx
+++ client/src/components/admin/trails/index.tsx
@@ -1,10 +1,79 @@
-import React from 'react';
+import { useMutation, useQuery } from '@apollo/client';
+import gql from 'graphql-tag';
+import React, { useState } from 'react';
+import { withRouter } from 'react-router';
+import useCurrentUser from '../../../hooks/useCurrentUser';
+import { PermissionTypes, Trail } from '../../../types/graphQLTypes';
+import StandardSearch from '../../sharedComponents/StandardSearch';
+import ListTrails from './ListTrails';
+
+const GET_FLAGGED_TRAILS = gql`
+  query ListFlaggedTrails {
+    trails: flaggedTrails {
+      id
+      name
+      flag
+    }
+  }
+`;
+
+const DELETE_TRAIL = gql`
+  mutation($id: ID!) {
+    deleteTrail(id: $id) {
+      id
+    }
+  }
+`;
+
+export interface SuccessResponse {
+  trails: Trail[];
+}
 
 const AdminTrails = () => {
-  return (
-    <h3>Trails</h3>
-  );
 
+  const user = useCurrentUser();
+
+  const {loading, error, data, client} = useQuery<SuccessResponse>(GET_FLAGGED_TRAILS);
+
+  const [searchQuery, setSearchQuery] = useState<string>('');
+
+  const [deleteTrailMutation] = useMutation(DELETE_TRAIL, {
+    onCompleted: client.resetStore,
+  });
+
+  const deleteTrail = (id: string) => {
+    if (user && user.permissions === PermissionTypes.admin) {
+      deleteTrailMutation({ variables: { id } });
+    }
+  };
+
+  const filterTrails = (value: string) => {
+    setSearchQuery(value);
+  };
+
+  return (
+    <>
+      <div>
+        <div>
+          <StandardSearch
+            placeholder={'Filter trails'}
+            setSearchQuery={filterTrails}
+            focusOnMount={false}
+            initialQuery={searchQuery}
+          />
+        </div>
+        <div>
+          <ListTrails
+            loading={loading}
+            error={error}
+            data={data}
+            deleteTrail={deleteTrail}
+            searchQuery={searchQuery}
+          />
+        </div>
+      </div>
+    </>
+  );
 };
 
-export default AdminTrails;
+export default withRouter(AdminTrails);
