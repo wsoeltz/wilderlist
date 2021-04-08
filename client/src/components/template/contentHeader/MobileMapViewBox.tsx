5c43177fea516a5c2a8d1ef58c521ff9b761ae69
Kyle Soeltz <wsoeltz@gmail.com>
2021-04-08 08:48:01 -0400

    Improve mobile map and scroll behavior

--- client/src/components/template/contentHeader/MobileMapViewBox.tsx
+++ client/src/components/template/contentHeader/MobileMapViewBox.tsx
@@ -1,8 +1,13 @@
-import React, {useEffect, useRef} from 'react';
+import { faAngleDoubleDown } from '@fortawesome/free-solid-svg-icons';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import debounce from 'lodash/debounce';
+import React, {useEffect, useRef, useState} from 'react';
 import {useLocation} from 'react-router-dom';
 import styled from 'styled-components/macro';
 import MapBoxLogoSVG from '../../../assets/images/mapbox-logo.svg';
+import useCurrentUser from '../../../hooks/useCurrentUser';
 import {mobileTopPadding} from '../../../styling/Grid';
+import {primaryColor} from '../../../styling/styleUtils';
 import MobileTab from './MobileTab';
 
 const LogoImg = styled.img`
@@ -15,6 +20,33 @@ const LogoImg = styled.img`
 
 const ViewBox = styled.div`
   position: relative;
+`;
+
+const removedScrollIconClassName = 'scroll-down-icon-scrolled-past-mid';
+
+const ScrollDownIcon = styled.button`
+  position: fixed;
+  bottom: 4.5rem;
+  left: 0;
+  right: 0;
+  z-index: 1000;
+  margin: 0 auto 1rem;
+  width: 2rem;
+  height: 2rem;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  border-radius: 1000px;
+  background-color: ${primaryColor};
+  color: #fff;
+  box-shadow: 0px 0px 5px 0px #525252;
+  pointer-events: all;
+  transition: opacity 0.1s ease-in-out;
+
+  &.${removedScrollIconClassName} {
+    opacity: 0;
+    pointer-events: none;
+  }
 `;
 
 const viewBoxId = 'mobile-map-viewbox-id';
@@ -30,7 +62,10 @@ export const useMapViewBoxScroll = (dependency: any) => {
 
 const MobileMapViewBox = () => {
   const mobileMidViewboxRef = useRef<HTMLDivElement | null>(null);
+  const contentTopRef = useRef<HTMLDivElement | null>(null);
+  const user = useCurrentUser();
   const location = useLocation();
+  const [scrollButtonVisible, setScrollButtonVisible] = useState<boolean>(true);
 
   useEffect(() => {
     const node = mobileMidViewboxRef.current;
@@ -39,19 +74,54 @@ const MobileMapViewBox = () => {
     }
   }, [mobileMidViewboxRef, location.pathname]);
 
+  useEffect(() => {
+    const node = contentTopRef.current;
+    const windowScrollFn = debounce(() => {
+      if (node) {
+        const pageScroll = document.documentElement.scrollTop || document.body.scrollTop;
+        const nodeTop = node.offsetTop;
+        if (pageScroll > nodeTop - 200) {
+          setScrollButtonVisible(false);
+          document.removeEventListener('scroll', windowScrollFn);
+        }
+      }
+    }, 250);
+    document.addEventListener('scroll', windowScrollFn);
+    return () => document.removeEventListener('scroll', windowScrollFn);
+  }, [contentTopRef]);
+
+  const onScrollButtonClick = () => {
+    const node = contentTopRef.current;
+    if (node) {
+      window.scrollTo({
+        top: node.offsetTop - 100,
+        left: 0,
+        behavior: 'smooth',
+      });
+    }
+  };
+
   return (
     <>
      <div
-       style={{height: '20vh'}}
+       style={{height: '8vh'}}
      />
      <ViewBox
        id={viewBoxId}
        ref={mobileMidViewboxRef}
-       style={{height: `calc(80vh - ${mobileTopPadding * 2.1}px)`}}
+       style={{height: `calc(80vh - ${(mobileTopPadding * 2.1)}px)`}}
      >
        <LogoImg src={MapBoxLogoSVG} alt={'Mapbox'} title={'Mapbox'} />
      </ViewBox>
+     <div ref={contentTopRef} />
      <MobileTab />
+      <ScrollDownIcon
+        style={user ? undefined : {bottom: '1.5rem'}}
+        onClick={onScrollButtonClick}
+        className={scrollButtonVisible ? undefined : removedScrollIconClassName}
+      >
+       <FontAwesomeIcon icon={faAngleDoubleDown} />
+      </ScrollDownIcon>
    </>
   );
 };
